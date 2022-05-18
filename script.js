fa01816935af1dd7a3caa2dad90f18df94f0ea25
KJ Johnson <kjohns95@emich.edu>
2022-05-17 20:26:16 -0400

    added submitPostButton in JS with submitPostHandler event function

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ cancelPostButton.addEventListener("click", () => {
   hiddenForm.classList.add("hidden");
   hiddenForm.classList.remove("show");
 });
+
+let submitPostButton = document.getElementById("submitPostButton");
+submitPostButton.addEventListener("click", submitPostHandler);
+function submitPostHandler(event) {
+  event.preventDefault();
+  //want to get input values of form
+  //want to create elements that will display values
+  //then hide form and reset form values
+  let postFormTitleValue = document.getElementById("title").value;
+  let postFormAuthorValue = document.getElementById("author").value;
+
+  let postFormContentValue = document.getElementById("content").value;
+  let postForm = document.getElementById("post-form");
+  postForm.classList.add("hidden");
+  postForm.classList.remove("show");
+
+  console.log(postFormAuthorValue);
+  console.log(postFormTitleValue);
+  console.log(postFormContentValue);
+
+  postForm.reset();
+}
